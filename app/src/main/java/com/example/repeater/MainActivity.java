b71ab294b6461df737684da305b6df862c53b822
SHIVAM3025 <shivamgupta1887@gmail.com>
2020-03-09 16:41:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/repeater/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.repeater;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int APP_PERMISSION_REQUEST = 102;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton= (Button) findViewById(R.id.createBtn);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, APP_PERMISSION_REQUEST);
+        } else {
+            initializeView();
+        }
+
+        initializeView();
+
+    }
+
+
+    private void initializeView() {
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, FloatWidgetService.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == APP_PERMISSION_REQUEST && resultCode == RESULT_OK) {
+            initializeView();
+        } else {
+            Toast.makeText(this, "Draw over other app permission not enable.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
